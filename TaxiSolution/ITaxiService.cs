c60bbf2777cd33a55b692dd4b9712748c6b7b794
eduardg2012 <35940088+eduardg2012@users.noreply.github.com>
2018-03-28 19:29:36 -0500

    Agregar archivos de proyecto.

---
+++ TaxiSolution/ITaxiService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TaxiSolution
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "ITaxiService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface ITaxiService
+    {
+        [OperationContract]
+        string Loguear(string usuario, string password);
+    }
+}
